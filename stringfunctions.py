4cfb828c72cfdcaec3ba2c971ba9e348fb25d2d1
SM Thahir <sm20818918@gmail.com>
2025-07-15 22:37:29 +0530

    commit
    on 15-7-25

---
+++ stringfunctions.py
@@ -0,0 +1,14 @@
+#1.capitalize
+str1 = 'this a string'
+print(str1)
+print(str1.capitalize())
+#2.len()
+print(len(str1))
+#3.center
+#stringname.center(len,char)
+print(str1.center(20)) 
+#4.endswith
+print(str1.endswith('g'))
+print(str1[1:10].endswith('r'))
+#stratswith
+print(str1.startswith("a",5))
