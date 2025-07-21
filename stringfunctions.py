a3987abea0aa1de0b6204abf935d977b95334bc2
SM Thahir <sm20818918@gmail.com>
2025-07-21 20:46:14 +0530

    commit
    on 21/7/25

--- stringfunctions.py
+++ stringfunctions.py
@@ -1,5 +1,5 @@
 #1.capitalize
-str1 = 'this a string'
+str1 = 'this a String'
 print(str1)
 print(str1.capitalize())
 #2.len()
@@ -12,3 +12,7 @@ print(str1.endswith('g'))
 print(str1[1:10].endswith('r'))
 #stratswith
 print(str1.startswith("a",5))
+#Uppercase
+print(str1.upper())
+#lowercase
+print(str1.lower())
